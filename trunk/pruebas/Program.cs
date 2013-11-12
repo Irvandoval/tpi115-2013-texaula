09d779da3ff7c15f88d775a3464c0e3c14a5e3f6
Irvandoval <Irvandoval@b444dd26-5412-799e-76d5-c53a1a6e1f60>
2013-11-12 18:20:36 +0000

    agregados metodos para listaEnfermera listaMedicos listaRecepcionistas listaTratamientos

---
+++ trunk/pruebas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SistemaHospital.Negocio;
+
+
+namespace pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         /*   DB n = new DB();
+           // n.conectar();
+            //int res = n.getIdUsuario("admin");
+            Console.WriteLine(n.estadoConexion);
+            Console.Read();
+            n.cerrar();*/
+
+           string s= Encriptador.RijndaelSimple.Encriptar("1234");
+           Console.WriteLine(s +" \n"+ Encriptador.RijndaelSimple.Desencriptar(s));
+
+           Console.Read();
+
+        }
+    }
+}
